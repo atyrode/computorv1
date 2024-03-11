cedf8081b41f8a7c2d203e9675ed730ac2b6da9f
atyrode <alex.tyrode@outlook.fr>
2024-03-11 16:23:51 +0100

    implement i/o module to print and prompt the user #2

---
+++ src/io.rs
@@ -0,0 +1,20 @@
+use std::io::{self, Write, Error};
+
+fn question_user(question: &str, prompt: &str) -> Result<(), Error> {
+    println!("{}", question);
+    print!("{}", prompt);
+    io::stdout().flush()?;
+    Ok(())
+}
+
+fn get_user_input() -> Result<String, Error> {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input)?;
+    Ok(input)
+}
+
+pub fn initial_prompt() -> Result<String, Error> {
+    question_user("Enter your polynomial equation:", "> ")?;
+    get_user_input()
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+mod io;
+use io::initial_prompt;
+
+
+fn computorv1() -> String {
+    let equation: String;
+
+    match initial_prompt() {
+        Ok(user_input) => equation = user_input,
+        Err(err) => return format!("{}", err),
+    }
+
+    equation
+}
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        println!("{}", "-".repeat(80));
+        let result: String = computorv1();
+        println!("{}", result);
+    }
 }
